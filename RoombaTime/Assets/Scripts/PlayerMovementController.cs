08f32a615f70129aa5f535fb825b7ed8b39db8bb
Jordi van der Plank <jordivdp1@gmail.com>
2022-11-06 15:47:51 +0100

    setup[gamescene]

--- RoombaTime/Assets/Scripts/PlayerMovementController.cs
+++ RoombaTime/Assets/Scripts/PlayerMovementController.cs
@@ -9,14 +9,28 @@ using UnityEngine.UI;
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
+    [SerializeField] float rotationSpeed;
     Vector2 movement = new Vector2();
     Rigidbody2D rb2D;
+
+    [SerializeField] Transform roombaSprite;
 
     public bool canMove;
 
     private void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //roombaSprite.rotation = movement;
+        Vector2 moveDirection = rb2D.velocity;
+        if (moveDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            roombaSprite.rotation = Quaternion.Lerp(roombaSprite.rotation, Quaternion.AngleAxis(angle - 90, Vector3.forward), rotationSpeed * Time.deltaTime);
+        }
     }
 
     void FixedUpdate()
